f98f8a6848838d8fd58db04ce218868896efc3a4
Anthony Hansord <tonyhansorddev@gmail.com>
2023-04-02 15:30:07 +1000

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'black', 'aggressive', true);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'yellow', 'calm', true);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ('Tabitha', 2, 'F', 'brown', 'calm', true);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'M', 'black', 'aggressive', true);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ('Melissa', 7, 'F', 'white', 'calm', true);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ('Frosty', 3, 'M', 'white', 'calm', true);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ('Greg', 3, 'F', 'brown', 'agrressive', true);
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES (null, 3, 'M', 'yellow', 'calm', true);
